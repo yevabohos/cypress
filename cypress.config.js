91e4883274af5b6b3d72fa2d66ec20995d33578a
yevabogos <yevabogos@gmail.com>
2022-07-22 17:46:37 +0300

    HW1

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,15 @@
-module.exports = {
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    baseUrl: "http://localhost:3000",
+    watchForFileChanges: false,
+    numTestsKeptInMemory: 50,
+    specPattern: "cypress/e2e/**/*.spec.{js,jsx,ts,tsx}",
+    retries: {
+      runMode: 2,
+      openMode: 1,
     },
+    setupNodeEvents(on, config) {},
   },
-};
+});
